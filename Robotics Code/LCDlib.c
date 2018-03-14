8764298af24e3803912ff84deed82bf588c251b1
CollinHeist <collinheist@gmail.com>
2018-03-13 19:04:26 -0700

    Finalized formatting of LCDlib.c
    Fixed the syntax and format of the file so that it is more consistent with the rest of the project

--- Robotics Code/LCDlib.c
+++ Robotics Code/LCDlib.c
@@ -1,33 +1,26 @@
-/* ************************************************************************** */
-/** Descriptive File Name: LCDlib.c - Character LCD driver using the PIC32
-                           PMP interface
-  @ Author  
-     Richard Wall
-  
-  @Summary
+/* ---------- Character LCD driver using the PIC32 PMP interface -------------
+  @ Summary
     Character LCD control using the PIC32 parallel Master Port (PMP).
-
-  @Description
-    This program provides low level LCD IO control using the PIC32
-    PMP bus. The initLCD function must be call before any ASCII
-    characters can be written to the LCD. The readLCD and writeLCD
-    functions handle access to both the LCD control registers
-    (addr = 0) and the LCD DDRAM/CGRAM (addr = 1). The putsLCD function
-    processes an ASCII string of characters that can contain ASCII
-    control characters LF, CR, and TAB.  The TAB control moves the
-    cursor 8 spaces to the right.
+  @ Description
+     This program provides low level LCD IO control using the PIC32
+     PMP bus. The initLCD function must be call before any ASCII
+     characters can be written to the LCD. The readLCD and writeLCD
+     functions handle access to both the LCD control registers
+     (addr = 0) and the LCD DDRAM/CGRAM (addr = 1). The putsLCD function
+     processes an ASCII string of characters that can contain ASCII
+     control characters LF, CR, and TAB.  The TAB control moves the
+     cursor 8 spaces to the right.
  
-    Macros provided in LCDlib.h provide LCD cursor control and character
-    based operations
+     Macros provided in LCDlib.h provide LCD cursor control and character
+     based operations
+  ---------------------------------------------------------------------------- */
 
-**************************************************************************** */
-
-/* System included files */
+// System included files
 #include "hardware.h"
 #include <plib.h>
 
-/* Application included files */
-#include "swDelay.h"		/* Required for DelayMs function */
+// Application included files
+#include "swDelay.h"    // Required for DelayMs function
 #include "LCDlib.h"
 #include <stdint.h>
 
@@ -41,44 +34,47 @@ unsigned char lcd_blks[64] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1F,
                               0x00, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F,
                               0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F};
 
-/* initLCD Function Description *********************************************
- * SYNTAX:          void initLCD( void);
- * KEYWORDS:        LCD, Character, PMP
- * DESCRIPTION:     Sets up the PMP interface and Initializes a 16x2
- *                  character LCD
- * PARAMETERS:      None
- * RETURN VALUE:    None
- * Notes:           None
- * END DESCRIPTION **********************************************************/
+/* ------------------------------- initLCD() ---------------------------------
+  @ Syntax
+     void initLCD(void);
+  @ Description
+     Sets up the PMP interface and Initializes a 16x2 character LCD
+  @ Parameters
+     None
+  @ Return Value
+     None
+  ---------------------------------------------------------------------------- */
 void initLCD(void) {
-
     int config1 = PMP_ON|PMP_READ_WRITE_EN|PMP_READ_POL_HI|PMP_WRITE_POL_HI;
     int config2 = PMP_DATA_BUS_8 | PMP_MODE_MASTER1 |
                   PMP_WAIT_BEG_4 | PMP_WAIT_MID_15 | PMP_WAIT_END_4;
-    int config3 = PMP_PEN_0;        /* only PMA0 enabled */
-    int config4 = PMP_INT_OFF;      /* no interrupts used */
+    int config3 = PMP_PEN_0;        // only PMA0 enabled
+    int config4 = PMP_INT_OFF;      // no interrupts used
 
-	PORTSetPinsDigitalIn (IOPORT_E, LCD_DATAbits);		// RE0:7
-	PORTSetPinsDigitalOut(IOPORT_D, ENpin);				// RD4
-	PORTSetPinsDigitalOut(IOPORT_D, RWpin);				// RD5
-	PORTSetPinsDigitalOut(IOPORT_B, RSpin);				// RB15
+	PORTSetPinsDigitalIn (IOPORT_E, LCD_DATAbits);	// RE0:7
+	PORTSetPinsDigitalOut(IOPORT_D, ENpin);		// RD4
+	PORTSetPinsDigitalOut(IOPORT_D, RWpin);		// RD5
+	PORTSetPinsDigitalOut(IOPORT_B, RSpin);		// RB15
 
-    mPMPOpen(config1, config2, config3, config4); // PMP initialization
+    mPMPOpen(config1, config2, config3, config4);       // PMP initialization
     
     msDelay (20); // wait for > 20ms
     msDelay (50); // Settling time - not required
 } // End of initLCD
 
-
-/* readLCD Function Description *******************************************
- * SYNTAX:          char readLCD( int addr);
- * KEYWORDS:        LCD, Character, PMP, read
- * DESCRIPTION:     Writes a single character from the LCD.
- * PARAMETER1:      addr - sets the RS pin
- * RETURN VALUE:    Byte read from LCD
- * Notes:           The two read cycles are required as per peripheral library
- * END DESCRIPTION **********************************************************/
-unsigned char readLCD(int addr) {
+/* ------------------------------- readLCD() ---------------------------------
+  @ Syntax
+     char readLCD(int addr);
+  @ Description
+     Writes a single character from the LCD.
+  @ Parameter
+     @ param1: addr - sets the RS pin
+  @ Return Value
+     Byte read from LCD
+  @ Notes
+     The two read cycles are required as per peripheral library
+  ---------------------------------------------------------------------------- */
+unsigned char readLCD (int addr) {
     unsigned char data; 
     PMPSetAddress(addr);            // select register
     data = mPMPMasterReadByte();    // initiate read sequence
@@ -86,40 +82,41 @@ unsigned char readLCD(int addr) {
     return data;
 } // End of readLCD
 
-/* writeLCD Function Description *******************************************
- * SYNTAX:          void writeLCD( int addr, char c);
- * KEYWORDS:        LCD, Character, PMP, write
- * DESCRIPTION:     Writes a single character to the LCD.
- * PARAMETER1:      addr - sets the RS pin
- * PARAMETER2:      c - 8 bit data to write to LCD
- * RETURN VALUE:    None
- * Notes:           This is a blocking function waiting for the LCD busy flag
-                    to be set clear.
-END DESCRIPTION ************************************************************/
-void writeLCD(int addr, char c) {
+/* ------------------------------- writeLCD() --------------------------------
+  @ Syntax
+     void writeLCD(int addr, char c);
+  @ Description
+     Writes a single character to the LCD.
+  @ Parameters
+     @ param1: addr - sets the RS pin
+     @ param2: c    - 8 bit data to write to LCD
+  @ Return Value
+     None
+  @ Notes
+     This is a blocking function waiting for the LCD busy flag to be set clear.
+  ---------------------------------------------------------------------------- */
+void writeLCD (int addr, char c) {
     PMPSetAddress(addr);    // select LCD register 
     nsDelay(10);            // Wait 10 ns
     PMPMasterWrite(c);      // initiate write sequence
 } // End of writeLCD
     
-
-/* putcLCD Function Description *******************************************
- * SYNTAX:          void putcLCD( int rs, char c);
- * KEYWORDS:        LCD, Character, write
- * DESCRIPTION:     Writes a single character to the LCD.
- * PARAMETER1:      the RS pin
- * PARAMETER2:      c - 8 bit data to write to LCD
- * RETURN VALUE:    None
- * NOTES:           This is a blocking function waiting for the LCD busy flag
- *                  to be set clear.
- *                  
- *                  The LCD data pins must use bit set and bit clear 
- *                  instructions to prevent altering PORT E pins that are not 
- *                  connected to the LCD.
- *                  
- *                  Implements minimum delays for setup and hold times.
- * 
-END DESCRIPTION ************************************************************/
+/* ------------------------------- putcLCD() --------------------------------
+  @ Syntax
+     void putcLCD(int rs, char c);
+  @ Description
+     Writes a single character to the LCD.
+  @ Parameters
+    @ param1: rs - the RS pin
+    @ param2: c  - 8 bit data to write to LCD
+  @ Return Value
+     None
+  @ Notes
+     This is a blocking function waiting for the LCD busy flag to be set
+     clear. The LCD data pins must use bit set and bit clear instructions
+     to prevent altering PORT E pins that are not connected to the LCD. 
+     Implements minimum delays for setup and hold times.
+  ---------------------------------------------------------------------------- */
 void putcLCD(int rs, char c) {
     char bf;
     {
@@ -132,29 +129,30 @@ void putcLCD(int rs, char c) {
     writeLCD(rs, c);
 } // End of putcLCD
 
-/* putsLCD Function Description ********************************************
- * SYNTAX:          void putsLCD( char *s);
- * KEYWORDS:        LCD, string, PMP, write
- * DESCRIPTION:     Writes a NULL terminated ASCII string of characters to
- *                  the LCD.
- * PARAMETER1:      character pointer to text string
- * RETURN VALUE:    None
- *
- * Notes:           LF is converted to CLEAR DISPLAY.  CR is converted to
- *                  LINE RETURN, TAB inserts SPACE character to the end 
- *                  of line.
- * END DESCRIPTION *********************************************************/
+/* ------------------------------- putsLCD() ---------------------------------
+  @ Syntax
+     void putsLCD(char *s);
+  @ Description
+     Writes a NULL terminated ASCII string of characters to the LCD.
+  @ Parameters
+    @ param1: character pointer to text string
+  @ Return Value
+     None
+  @ Notes
+     LF is converted to CLEAR DISPLAY.  CR is converted to LINE RETURN,
+     TAB inserts SPACE character to the end of line.
+  ---------------------------------------------------------------------------- */
 void putsLCD(char *s) {  
     char c;
     while (*s) {
         switch (*s) {           // Check for ASCII control characters
         case '\n':              // point to second line
             waitLCD();          // Ensure cursor addr is valid
-            setLCDC( 0x40);
+            setLCDC(0x40);
             break;
         case '\r':              // home, point to first line 
             waitLCD();          // Ensure cursor addr is valid
-            setLCDC( 0);
+            setLCDC(0);
             break;
         case '\t':              // advance next TAB (8) positions 
             waitLCD();          // Ensure cursor address is valid
@@ -167,7 +165,7 @@ void putsLCD(char *s) {
             }
 
             if ((c >= LCD_LINE1) && (c < LCD_LINE2)) { //if necessary reposition
-                setLCDC( LCD_LINE2 );   // Reposition cursor
+                setLCDC(LCD_LINE2);   // Reposition cursor
             }
             break;                
         default:                // print character
@@ -175,25 +173,25 @@ void putsLCD(char *s) {
             c = addrLCD();      // Get cursor address
             if (( c >= LCD_LINE1) && (c < LCD_LINE2)) { //if necessary reposition
                 waitLCD();              // Ensure cursor address is valid
-                setLCDC( LCD_LINE2 );   // Reposition cursor
+                setLCDC(LCD_LINE2);   // Reposition cursor
             }
             putcLCD(LCDDATA, *s);       // BF is checked before writing char
             break;
         } // end switch-case
         s++;
-    } /* End while */
+    } // End while
 } // End of putsLCD
 
-/* initLCDGr Function Description ********************************************
- * SYNTAX:          void initLCDGr(void);
- * KEYWORDS:        LCD, string, PMP, write, variable CGRAM
- * DESCRIPTION:     Programs the 8 CGRAM characters FFT Display
- *
- * PARAMETER1:      None
- * RETURN VALUE:    None
- *
- * Notes:           None
- * END DESCRIPTION *********************************************************/
+/* ------------------------------ initLCDGr() --------------------------------
+  @ Syntax
+     void initLCDGr(void);
+  @ Description
+     Programs the 8 CGRAM characters FFT Display
+  @ Parameter
+     None
+  @ Return Value
+     None
+  ---------------------------------------------------------------------------- */
 void initLCDGr(void) {
     int i;
     clrLCD();
@@ -201,9 +199,9 @@ void initLCDGr(void) {
         writeLCD(LCDDATA, i);
     }
 
-// Program graphical characters
+//  Program graphical characters
     setLCDG(0); // Set CGRAM pointer to 0
-    for (i=0; i<64; i++) {
+    for (i = 0; i < 64; i++) {
         putcLCD(LCDDATA, lcd_blks[i]);
     }
     gotoLCD(0); // Set DDRAM to 0
@@ -212,24 +210,24 @@ void initLCDGr(void) {
     }
 }
 
-/* FFT_Disp Function Description ********************************************
- * SYNTAX:          void FFT_Disp(int16_t* mag, int N);
- * KEYWORDS:        LCD, string, PMP, write, variable CGRAM, graphic characters
- * DESCRIPTION:     Display the 16 FFT bins using the 8 graphical characters
- *
- * PARAMETER1:      Array containing the bin magnitudes
- * PARAMETER2:      Number of bins to display
- * RETURN VALUE:    None
- *
- * Notes:           None
- * END DESCRIPTION *********************************************************/
-void FFT_Disp(int16_t* mag, int N) {
+/* ------------------------------ FFT_Disp() ---------------------------------
+  @ Syntax
+     void FFT_Disp(int16_t* mag, int N);
+  @ Description
+     Display the 16 FFT bins using the 8 graphical characters
+  @ Parameters
+    @ param1: mag - Array containing the bin magnitudes
+    @ param2: N   - Number of bins to display
+  @ Return Value
+     None
+  ---------------------------------------------------------------------------- */
+void FFT_Disp (int16_t* mag, int N) {
     int i;
     uint8_t amp[32];
     uint8_t a;
 
     clrLCD();
-    for (i = 0; i< N; i++) {
+    for (i = 0; i < N; i++) {
         a = (uint8_t) (mag[i]);
         if (a > 7) {
             amp[i+16] = 7;
@@ -240,23 +238,21 @@ void FFT_Disp(int16_t* mag, int N) {
             amp[i] = 0;           
         }
     }
-    for (i = 0; i< N; i++) {
+    for (i = 0; i < N; i++) {
         if (amp[i] == 0) {
-            putcLCD( LCDDATA, ' ');
+            putcLCD(LCDDATA, ' ');
         }
         else {
-            putcLCD( LCDDATA, amp[i]);            
+            putcLCD(LCDDATA, amp[i]);            
         }
     }
-    gotoLCD(NEW_LINE);
-    for (i = 0; i< N; i++) {
+    gotoLCD (NEW_LINE);
+    for (i = 0; i < N; i++) {
         if (amp == 0) {
-            putcLCD( LCDDATA, ' ');
+            putcLCD(LCDDATA, ' ');
         }
         else {
-            putcLCD( LCDDATA, amp[i+16]);            
+            putcLCD(LCDDATA, amp[i+16]);            
         }
     }
 }
-
-/* End of LCDlib.c */
