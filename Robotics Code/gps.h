1e4dcac1a8e828c2f2baefccd8e9a6c698d64c70
CollinHeist <collinheist@gmail.com>
2018-03-13 22:34:49 -0700

    Created the header file of gps.c
    Haven't yet added a majority of the header's code, but created the basic outline

---
+++ Robotics Code/gps.h
@@ -0,0 +1,15 @@
+/* ------------------- Assignment of glboal variables ------------------------ */
+
+#ifndef _MAIN_H    // Guard against multiple inclusion 
+#define _MAIN_H
+
+    #define GPS_BUFFER_SIZE 256
+
+    // GPS Sentence type
+    #define GPRMC           1
+    #define GPGGA           2
+    #define GPGLL           3
+
+#endif /* _MAIN_H */
+
+// Functions in gps.c
