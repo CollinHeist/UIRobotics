0b310639ab6fe4a3ba319c9276c5045e35c6f4ba
Collin Heist <collinheist@gmail.com>
2018-04-13 21:41:18 -0700

    Added powerMotors()
    Added in the powerMotors function, which calls runMotor on each motor individually, so that with one function call we can update the power-levels to each motor.

--- Robotics Code/motor.c
+++ Robotics Code/motor.c
@@ -13,7 +13,6 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#include "led7.h"
 #include "comm_lib.h"
 #include "swDelay.h"
 #include "RC.h"
@@ -22,8 +21,8 @@
   @ Syntax
      int isValid = runMotor(int channel, int position);
   @ Summary
-     Public function that should be called to update the power of the motor
-     specified in 'channel'. This power level will be set to 'position'.
+     Function that should be called to update the powers of the individual
+	 motors specified in 'channel'. This power level will be set to 'position'.
      This function presently only sends one PWM signal, but should the motors
      require a second signal after a short delay, there is commented code for
      that.
@@ -41,7 +40,7 @@
   ---------------------------------------------------------------------------- */
 int runMotor(int channel, int position) {
     initRC();			// Initialize RC channel output pins
-	DelayMs(250);		// Wait a quarter of a second
+	DelayMs(125);		// Wait an eighth of a second
 	    
     /* ------- Make sure the provided RC Channel and position are valid ------ */
     unsigned int pos = position + 100 / 2; // Scale the % range to the PWM range
@@ -68,7 +67,52 @@ int runMotor(int channel, int position) {
     else { return 0; }
 }
 
-/* --------------------------- system_init() ---------------------------------
+/* ----------------------------- powerMotors() -------------------------------
+  @ Syntax
+     powerMotors(int leftMotor, int rightMotor, float duration);
+  @ Summary
+     Public function that should be called by outside code, powers each
+	 of the two motors at the specified power levels for the specified
+	 duration. This code assumes that the left motor is on the first channel
+  @ Parameters
+     param1 : A array of length 2, where [0] is the channel of the left
+	          motor, and [1] is the power level, between -100% and 100%,
+			  at which to power the left motor.
+     param2 : A array of length 2, where [0] is the channel of the right
+	          motor, and [1] is the power level, between -100% and 100%,
+			  at which to power the right motor.
+	 param3 : The duration (in seconds) to power the motors at. At the end
+	 	      of this duration, the motors are returned to off.
+  @ Returns
+     1 : Both of the motors were powered successfully. No errors from runMotor().
+	 0 : Error code. One or both of the motors were unable to be powered at the
+	     provided levels for the provided time, OR the provided input arrays
+		 were not the proper dimensions.
+  ---------------------------------------------------------------------------- */
+int powerMotors(int[] leftMotor, int[] rightMotor, float duration) {
+	/* -- Throw an error code if the passed values are incorrectly bounded --- */
+	if (sizeof(leftMotor)  / sizeof(leftMotor[0] != 2  || 
+        sizeof(rightMotor) / sizeof(rightMotor[0] != 2 ||
+        duration <= 0) { return 0; }
+	
+	/* ----------------- Start and wait time - in milliseconds --------------- */
+	unsigned int tWait, tStart;
+	tStart = ReadCoreTimer();
+	tWait  = CORE_MS_TICK_RATE * duration * 1000;
+	
+	/* ------ Power the motors at the passed values, for the passed time ----- */
+	int leftMotorSuccess  = runMotor(leftMotor[0],  leftMotor[1]);
+	int rightMotorSuccess = runMotor(rightMotor[0], rightMotor[1]);
+	while (ReadCoreTimer() - tStart < tWait);
+	
+	/* -------- Turn off the motors by powering them to a level of 0% -------- */
+	int tempL = runMotor(leftMotor[0], 0);
+	int tempR = runMotor(rightMotor[0], 0);
+	
+	return (leftMotorSuccess && rightMotorSuccess);
+}
+
+/* ---------------------------- system_init() ---------------------------------
   @ Syntax
      void initRC()
   @ Summary
@@ -78,11 +122,13 @@ int runMotor(int channel, int position) {
   ---------------------------------------------------------------------------- */
 void initRC (void) {
     #define TMR2_TICK	25000
-	cfgRC1();		// Set RC pins for PWM output
+	/* --------------------- Set RC pins for PWM Output ---------------------- */
+	cfgRC1();
 	cfgRC2();
 
     OpenTimer2(T2_ON | T2_SOURCE_INT | T2_PS_1_4, PWM_PD-1);
-	// Set Timer 2 interrupt with a priority of 2
+	
+	/* ------------- Set Timer 2 interrupt with a priority of 2 -------------- */
     ConfigIntTimer2(T2_INT_ON | T2_INT_PRIOR_2);
     
     OpenOC5((OC_ON|OC_TIMER_MODE16|OC_TIMER2_SRC|OC_PWM_FAULT_PIN_DISABLE), 
@@ -93,14 +139,13 @@ void initRC (void) {
 
 /* -------------------------------- ISR --------------------------------------
   @ Syntax
-     Automatically called function, no need to reference this
+     Automatically called function, no need to reference this, but honestly
+	 I have no idea what it does, and am afraid to delete it :)
   @ Summary
-     None
+     Clears the interrupt flag (whatever that means)
   @ Returns
      None
   @ Returns
      None
   ---------------------------------------------------------------------------- */
-void __ISR(_TIMER_2_VECTOR, IPL2SOFT) Timer2Handler(void) {
-    mT2ClearIntFlag();			// clear the interrupt flag
-}
+void __ISR(_TIMER_2_VECTOR, IPL2SOFT) Timer2Handler(void) { mT2ClearIntFlag(); }
