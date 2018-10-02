3ee3447d4546c0c8bac1f6e93df5f2fd9bdd8be9
Collin Heist <collinheist@gmail.com>
2018-10-02 13:32:56 -0700

    Added final version of NAV project

---
+++ NAV1.X/build/default/production/_ext/1472/TMP03.o.d
@@ -0,0 +1,4 @@
+build/default/production/_ext/1472/TMP03.o:  \
+../TMP03.c  \
+../hardware.h  \
+ ../TMP03.h 
---
+++ NAV1.X/build/default/production/_ext/1472/main.o.d
@@ -0,0 +1,15 @@
+build/default/production/_ext/1472/main.o:  \
+../main.c  \
+../config_bits.h  \
+ ../hardware.h  \
+../LCDlib.h  \
+../swDelay.h  \
+../led7.h  \
+../comm_lib.h  \
+../main.h  \
+ ../DHT.h  \
+../TMP03.h  \
+../GPS_I2C.h  \
+../I2C_lib.h  \
+../MAG3110.h  \
+../i2c_lib.h 
