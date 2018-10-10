7ebaeffa87f0d739dbe4ccc9740da6d3c2b7a9d0
Collin Heist <collinheist@gmail.com>
2018-10-09 20:06:52 -0700

    Update swDelay.c

--- Robotics Code/swDelay.c
+++ Robotics Code/swDelay.c
@@ -24,7 +24,7 @@
     This is a millisecond delay function uses the core time to set the base
     millisecond delay period. Delay periods of zero are permitted. 
  @ Parameters
-    @ param1 : mS - the number of milliseconds to delay
+    param1 : mS - the number of milliseconds to delay
  @ Return Value
     None
  @ Notes
@@ -32,17 +32,17 @@
     interrupt service routines in preemptive systems.
  ----------------------------------------------------------------------------- */
 void DelayMs(unsigned int mS) {
-    unsigned int tWait, tStart;
-    tStart = ReadCoreTimer();
-    tWait = (CORE_MS_TICK_RATE * mS);
-    while ((ReadCoreTimer()- tStart) < tWait);
+	unsigned int tWait, tStart;
+	tStart = ReadCoreTimer();
+	tWait  = (CORE_MS_TICK_RATE * mS);
+	while ((ReadCoreTimer() - tStart) < tWait);
 }
 
 /* ------------------------------ DelayUs() ----------------------------------
  @ Syntax
     DelayUs(unsigned int us);
  @ Parameters
-    @ param1 : us - the number of microseconds to delay
+    param1 : us - the number of microseconds to delay
  @ Descriptions
    This is a microsecond delay function that will repeat a specific number of times.  
  @ Return Value
@@ -52,19 +52,17 @@ void DelayMs(unsigned int mS) {
     interrupt service routines in preemptive systems.
  ----------------------------------------------------------------------------- */
 void DelayUs(unsigned int us) {
-    unsigned int tWait, tStart;
-    tStart = ReadCoreTimer();
-    tWait = (CORE_MS_TICK_RATE * us / 1000);
-    while ((ReadCoreTimer()- tStart) < tWait);
+	unsigned int tWait, tStart;
+	tStart = ReadCoreTimer();
+	tWait  = (CORE_MS_TICK_RATE * us / 1000);
+	while ((ReadCoreTimer() - tStart) < tWait);
 }
 
 /* ------------------------------ DelayNs() ----------------------------------
  @ Syntax
     DelayNs(unsigned int ns);
  @ Parameter
-    @ param1 : ns - the number of 10's of microseconds to delay
- @ Keywords
-    delay, ns, microseconds, software delay, core timer
+    param1 : ns - the number of 10's of microseconds to delay
  @ Description
     This is a 10 nanosecond delay function that will repeat a specified
     number of times.  
@@ -75,10 +73,10 @@ void DelayUs(unsigned int us) {
     to execute interrupt service routines in preemptive systems.
  ---------------------------------------------------------------------------- */
 void DelayNs(unsigned int ns) {
-    unsigned int tWait, tStart;
-    tStart = ReadCoreTimer();
-    tWait = (CORE_MS_TICK_RATE * ns / 10000);
-    while((ReadCoreTimer()- tStart) < tWait);
+	unsigned int tWait, tStart;
+	tStart = ReadCoreTimer();
+	tWait = (CORE_MS_TICK_RATE * ns / 10000);
+	while ((ReadCoreTimer() - tStart) < tWait);
 }
 
 /* ------------------------------ PeriodMs() ---------------------------------
@@ -90,7 +88,7 @@ void DelayNs(unsigned int ns) {
     This is a non-blocking millisecond delay function that can be use for
     periodic activation of code.
  @ Parameter
-    @ param1 : int msec - the number of milliseconds to delay
+    param1 : int msec - the number of milliseconds to delay
  @ Return Value
     integer - delay in progress TRUE / FALSE
  @ Notes
@@ -100,17 +98,17 @@ void DelayNs(unsigned int ns) {
     time to execute interrupt service routines in preemptive systems.
  ---------------------------------------------------------------------------- */
 int PeriodMs(unsigned int msec) {
-    static int delay_in_progress_flag = FALSE;
-    static unsigned int tWait, tStart;
+	static int delay_in_progress_flag = FALSE;
+	static unsigned int tWait, tStart;
 
-    if (delay_in_progress_flag == 0) {       // New Period
-        tStart = ReadCoreTimer();            // Capture starting time
-        tWait = (CORE_MS_TICK_RATE * msec);  // Compute delay period
-        delay_in_progress_flag = TRUE;       // Set operation flag
+	if (delay_in_progress_flag == 0) {       // New Period
+		tStart = ReadCoreTimer();            // Capture starting time
+		tWait = (CORE_MS_TICK_RATE * msec);  // Compute delay period
+		delay_in_progress_flag = TRUE;       // Set operation flag
     }
+
+	if ((ReadCoreTimer() - tStart) >= tWait) // Check if period is over
+			delay_in_progress_flag = FALSE;
     
-    if ((ReadCoreTimer() - tStart) >= tWait) // Check if period is over
-        delay_in_progress_flag = FALSE;
-    
-    return delay_in_progress_flag;
+	return delay_in_progress_flag;
 }
