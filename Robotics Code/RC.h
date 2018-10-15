4d711b06dabfbfd612968444924bb6093b38d756
Collin Heist <collinheist@gmail.com>
2018-10-15 11:33:16 -0700

    Updated RC header file for new RC functionality

--- Robotics Code/RC.h
+++ Robotics Code/RC.h
@@ -1,21 +1,24 @@
-#ifndef __RC__
-	#define __RC__
+#ifndef _RC_H
+	#define _RC_H
 
-    #define cfgRC2()    do {PORTSetPinsDigitalOut(IOPORT_B, BIT_8); RPB8R = 0x0B;} while(0)  // BTNR - OC5
-    #define cfgRC1()    do {PORTSetPinsDigitalOut(IOPORT_A, BIT_15); RPA15R = 0x0B;} while(0) // BTND - OC4
+	/* ----------- Macros for configuring RC pins as Digital Outputs --------- */
+    #define cfgRC1()    PORTSetPinsDigitalOut(IOPORT_D, BIT_9)
+    #define cfgRC2()    PORTSetPinsDigitalOut(IOPORT_D, BIT_11)
+	#define cfgRC3()    PORTSetPinsDigitalOut(IOPORT_D, BIT_10)
+    #define cfgRC4()    PORTSetPinsDigitalOut(IOPORT_D, BIT_8)
 
     /* -------------------- RC Channel pin assignment macros ----------------- */
-	#define RC_1(a)     SetDCOC4PWM(a); //LATBbits.LATB8 = a
-	#define RC_2(a) 	SetDCOC5PWM(a); //LATAbits.LATA15 = a
-	#define NRC		2	                // Number of RC channels
+	#define RC_1(a)     LATDbits.LATD9  = a
+	#define RC_2(a) 	LATDbits.LATD11 = a
+	#define RC_3(a) 	LATDbits.LATD10 = a
+	#define RC_4(a) 	LATDbits.LATD8  = a
+
+	#define NRC		4		// Number of RC channels
 
     /* --------- RC Setting Range - How wide the PWM Signal should be -------- */
-    #define PWM_RATE    50
-    #define PWM_PD      GetPeripheralClock() / 4 / PWM_RATE
-	#define RC_MIN		(PWM_PD / 20)
-	#define	RC_MAX		(PWM_PD / 10 )
-	#define RC_SPAN		(RC_MAX - RC_MIN)
-	#define RC_CENTER   (RC_MIN + (RC_SPAN / 2))
+    #define RC_MIN		100
+	#define RC_MAX		200
+	#define RC_SPAN		100
 
     /* ---------------------- Public Function declarations ------------------- */
     void initRC(void);
