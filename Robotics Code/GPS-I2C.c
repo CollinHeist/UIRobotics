73e4a11fdeea90d3658c71e3c6e5701e6aabcd6c
Collin Heist <collinheist@gmail.com>
2018-10-15 11:03:09 -0700

    Added new GPS Functionality

--- Robotics Code/GPS-I2C.c
+++ Robotics Code/GPS-I2C.c
@@ -3,11 +3,34 @@
 #include "swDelay.h"
 #include "GPS_I2C.h"
 #include "I2C_lib.h"
+#include "Uno.h"	// I think this might be an error? Ask Dr. Wall
 
-#include <plib.h>
+#ifdef WF32
+	#include <plib.h>
+#endif
+
 #include <string.h>
 #include <stdio.h>
 #include <stdint.h>
+
+/* ------------------------------- setGPS_RMC --------------------------------
+  @ Syntax
+     I2C_result r = setGPS_RMC();
+  @ Summary
+     Configures the GPS to only send the $RMC Packet, reduces I2C parsing needed
+  @ Parameters
+     None
+  @ Returns
+     I2C_result : I believe this is a flag ensuring the communication was successful
+  ---------------------------------------------------------------------------- */
+I2C_RESULT setGPS_RMC(void) {
+    I2C_RESULT i2cFlag;
+    int dly = 0;
+    do {
+        i2cFlag = sendMTKpacket(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+        DelayMs(MAXWAITSENTENCE);
+    } while (dly);
+}
 
 /* ----------------------------- GPS_DECODE_RMC ------------------------------
   @ Syntax
@@ -32,8 +55,8 @@ int GPS_DECODE_RMC(BYTE *str) {
         if (eol != NULL) {
             len = (int) (eol - s2);
             if (len > 70) {	// Test for full message
-                s2[len-1] = 0;
-				// printf("%s --> %d \n\r", s2, len-1);
+                s2[len - 1] = 0;
+				printf("%s --> %d \n\r", s2, len - 1);
 
 				// Read the serial communication, store results in corresponding gps struct
                 sscanf(s2,"$GNRMC,%d.000,%c,%f,%c,%f,%c,%f,%f,%d,,,%c*%x",\
@@ -42,9 +65,80 @@ int GPS_DECODE_RMC(BYTE *str) {
                            &gps.cksum);
                 invLED7();
             }
-            else
+            else {
+				printf("%s --> %d \n\r", s2, len - 1);
                 len = 0;
+			}
         }
     }
     return len;
+}
+
+/* -------------------------------- ReportGPS --------------------------------
+  @ Syntax
+     I2C_RESULT t - ReportGPS(FALSE);
+  @ Summary
+     Reads from the GPS, and returns the value as a result
+  @ Description
+     Function for actually filling a struct with GPS results
+  @ Parameters
+     param1 : Boolean on whether or not to output each GPS read
+  @ Returns
+     I2C_RESULT : Struct to fill with GPS results
+  ---------------------------------------------------------------------------- */
+I2C_RESULT ReportGPS (int show) {
+	I2C_RESULT i2cFlag;
+	int len = 255;
+	static int hour, min, sec;
+	int pkt_len = 0;
+
+	DelayMs(5);     // Give GPS time to up load new buffer
+	len = MAX_PACKET_SIZE;
+	i2cFlag = I2C_Read(I2C2, GPS_DEV_ID, gpsStr, &len);
+
+	if (i2cFlag == I2C_SUCCESS && (len > 73) && (len < 256)); {
+		LD1_IO = !LD1_IO;
+		gpsStr[len] = 0;
+		pkt_len = GPS_DECODE_RMC(gpsStr);
+		if (show) {
+			hour = (gps.utc_time / 10000);
+			min = (gps.utc_time - hour*10000)/100;
+			sec = (gps.utc_time - (hour*10000 + min*100));
+			hour -= 7;
+			if (hour < 0)
+				hour += 24;        
+			printf("Time: %2d:%2d:%2d  LAT:%f  LON:%f\n\r",\
+					hour, min, sec, gps.lon, gps.lat);
+		}
+	}
+        
+	return i2cFlag;   
+}
+
+/* ------------------------------ sendMTKpacket ------------------------------
+  @ Syntax
+     I2C_RESULT r = sendMTKpacket
+  @ Summary
+     Writes a provided char[] packet to the GPS over I2C
+  @ Parameters
+     param1 : Character array to be sent to the GPS - Max of 255 characters
+  @ Returns
+     I2C_RESULT : Either an error or success flag from the GPS, if write was sucessful
+  ---------------------------------------------------------------------------- */
+I2C_RESULT sendMTKpacket (char *packet) {
+	I2C_RESULT i2cFlag = I2C_SUCCESS;
+	int len;
+
+	len = strlen(packet);
+	// If the packet is >255 characters, error and return. Otherwise, send to the GPS
+	if (len > 255) {
+		printf("Packet too long, cannot be sent\n\r");
+		i2cFlag = I2C_ERROR;
+	}
+	else {
+		printf("GPS command: %s", packet );
+		i2cFlag = I2C_Write(I2C2, GPS_DEV_ID, packet, &len);
+	}
+	DelayMs(MAXWAITSENTENCE);
+	return i2cFlag;
 }
