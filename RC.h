b79409ff515552290f6abb6b5651237dbab8be2e
Collin Heist <collinheist@gmail.com>
2018-10-16 23:01:59 -0700

    Updated RC.h file for latest NAV project

--- RC.h
+++ RC.h
@@ -40,15 +40,15 @@
     /*  A brief description of a section can be given directly below the section
         banner.
      */
-    #define cfgRC1()    PORTSetPinsDigitalOut(IOPORT_D, BIT_9)
-    #define cfgRC2()    PORTSetPinsDigitalOut(IOPORT_D, BIT_11)
-    #define cfgRC3()    PORTSetPinsDigitalOut(IOPORT_D, BIT_10)
-    #define cfgRC4()    PORTSetPinsDigitalOut(IOPORT_D, BIT_8)
+    #define cfgRC1()    PORTSetPinsDigitalOut(IOPORT_D, BIT_4) // 10 PORTSetPinsDigitalOut(IOPORT_D, BIT_9)
+    #define cfgRC2()    PORTSetPinsDigitalOut(IOPORT_G, BIT_8) // 11 PORTSetPinsDigitalOut(IOPORT_D, BIT_11)
+    #define cfgRC3()    PORTSetPinsDigitalOut(IOPORT_G, BIT_7) // 12 PORTSetPinsDigitalOut(IOPORT_D, BIT_10)
+    #define cfgRC4()    PORTSetPinsDigitalOut(IOPORT_G, BIT_6) // 13 PORTSetPinsDigitalOut(IOPORT_D, BIT_8)
 // RC Channel pin assignment macros
-	#define RC_1(a)     LATDbits.LATD9 = a
-	#define RC_2(a) 	LATDbits.LATD11 = a
-	#define RC_3(a) 	LATDbits.LATD10 = a
-	#define RC_4(a) 	LATDbits.LATD8 = a
+	#define RC_1(a)     LATDbits.LATD4 = a
+	#define RC_2(a) 	LATGbits.LATG8 = a
+	#define RC_3(a) 	LATGbits.LATG7 = a
+	#define RC_4(a) 	LATGbits.LATG6 = a
 
 	#define NRC		4	// Number of RC channels
 
@@ -100,9 +100,11 @@
      */
     
 	void initRC(void);
-    void set_RC(int ch, int ctrl);
-	int check_switches(void);
+    void rc_output(int ch, int ctrl);
+    void rcUpdate(void);
+    void set_rc(int rc1, int rc2, int rc3, int rc4);
 
+    
 #endif /* _RC_H */
 
 /* *****************************************************************************
