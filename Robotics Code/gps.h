ffc60640270a1148d1c233488edcac3002d4aaaf
Collin Heist <collinheist@gmail.com>
2018-10-21 23:51:33 -0700

    Update gps.h

--- Robotics Code/gps.h
+++ Robotics Code/gps.h
@@ -1,15 +1,12 @@
-/* ------------------- Assignment of glboal variables ------------------------ */
+/* ---------------------- Guard against multiple inclusion ------------------- */
+#ifndef _MAIN_H
+	#define _MAIN_H
 
-#ifndef _MAIN_H    // Guard against multiple inclusion 
-#define _MAIN_H
+	#define GPS_BUFFER_SIZE 256
 
-    #define GPS_BUFFER_SIZE 256
+	// GPS Sentence type
+	#define GPRMC           1
+	#define GPGGA           2
+	#define GPGLL           3
 
-    // GPS Sentence type
-    #define GPRMC           1
-    #define GPGGA           2
-    #define GPGLL           3
-
-#endif /* _MAIN_H */
-
-// Functions in gps.c
+#endif
