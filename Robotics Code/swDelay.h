6a0d83ea5696f4a8f06d39d1454e162fbf01196c
Collin Heist <collinheist@gmail.com>
2018-10-25 21:40:05 -0700

    Finalized swDelay.h

--- Robotics Code/swDelay.h
+++ Robotics Code/swDelay.h
@@ -1,10 +1,15 @@
 /* ---------------------- Guard against multiple inclusion ------------------- */
-#ifndef _SW_DELAY_H
-    #define _SW_DELAY_H
+#ifndef __SW_DELAY_H__
+	#define __SW_DELAY_H__
 
-    /* ---------------------- Public Function declarations ------------------- */
-    void DelayMs(unsigned int mS);
-    void DelayUs(unsigned int us);
-    void DelayNs(unsigned int ns);
-    int PeriodMs(unsigned int msec);
+	/* ----------------- Necessary system include statements ----------------- */
+	#include <plib.h>
+
+	#include "hardware.h"
+
+	/* ---------------------- Public Function declarations ------------------- */
+	void DelayMs(unsigned int mS);
+	void DelayUs(unsigned int us);
+	void DelayNs(unsigned int ns);
+	int PeriodMs(unsigned int msec);
 #endif
