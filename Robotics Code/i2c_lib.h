6369dcf6a2776349eb946bc6151388de0a9b2ffd
Collin Heist <collinheist@gmail.com>
2018-10-25 21:50:28 -0700

    Created i2c_lib.h

---
+++ Robotics Code/i2c_lib.h
@@ -0,0 +1,22 @@
+/* --------------------- Guard Against Multiple Inclusions ------------------- */
+#ifndef __I2C_LIB_H__
+	#define __I2C_LIB_H__
+
+	/* ----------------- Necessary system include statements ----------------- */
+	#include <plib.h>
+	#include <string.h>
+	#include <stdio.h>
+	#include <stdint.h>
+
+	#include "hardware.h"
+	#include "swDelay.h"
+
+	/* ----------------- Public Global Variables / Constants ----------------- */
+	#define I2C_SPEED_STANDARD		100000
+	#define I2C_SPEED_FAST			400000
+
+	/* ---------------------- Public Function Declarations ------------------- */
+	I2C_RESULT I2C_Init(I2C_MODULE i2c_port,  int speed);
+	I2C_RESULT I2C_Read(I2C_MODULE i2c_port, BYTE DeviceAddress, BYTE *str, int *len);
+	I2C_RESULT I2CReadRegs(I2C_MODULE i2c_port, BYTE DeviceAddress, int reg_addr, BYTE *i2cData, int len);
+#endif
