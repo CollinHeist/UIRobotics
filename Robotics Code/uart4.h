9d38470e167ebe1d35e33001387d3e3c2c29ac65
Collin Heist <collinheist@gmail.com>
2018-10-25 21:36:19 -0700

    Finalized uart4.h

--- Robotics Code/uart4.h
+++ Robotics Code/uart4.h
@@ -1,30 +1,24 @@
-/* --------- UART Serial communications header file for comm_lib.c -----------
-   ----------- This declares all public functions and constants --------------
-  @ Summary
-     UART 4 initialization and character and string I/O.
-  @ Description
-     UART initialization can be set for any BAUD rate using EVEN, ODD, or NO 
-     parity. Character and string input functions are non blocking functions.
-     The serial interface uses UART channel 4.
-  @ Remarks
-     This code also uses the "printf" function on UART Serial Port 4
-  ---------------------------------------------------------------------------- */
+/* --------------------- Guarding against multiple inclusion ----------------- */
+#ifndef __UART4_H__
+	#define __UART4_H__
 
-#ifndef __MX370_COMM_H__
-    #define __MX370_COMM_H__
-    #define _UART4
+	/* ----------------- Necessary system include statements ----------------- */
+	#include <plib.h>
+	#include <stdio.h>
 
-    /* -------------------- ASCII Control Characters ------------------------- */
-    #define BACKSPACE       0x08
-    #define NO_PARITY       0
-    #define ODD_PARITY      1
-    #define EVEN_PARITY     2
+	#include "hardware.h"
+
+	/* ----------------- Public Global Variables / Constants ----------------- */
+	#define BACKSPACE	0x08
+	#define NO_PARITY	0
+	#define ODD_PARITY	1
+	#define EVEN_PARITY	2
+
+	/* ---------------------- Public Function declarations ------------------- */
+	void uart4_init(unsigned int baud, int parity);
+	void _mon_putc(char c);
+	BOOL putcU4(int c);
+	BOOL getcU4(char *ch);
+	int putsU4(const char *s); 
+	int getstrU4(char *s, unsigned int len);
 #endif
-
-/* --------------------------- Function declarations ------------------------- */
-void uart4_init(unsigned int baud, int parity);
-void _mon_putc(char c);
-BOOL putcU4(int ch);
-BOOL getcU4(char *ch);
-int putsU4(const char *s); 
-int getstrU4(char *s, unsigned int len);
