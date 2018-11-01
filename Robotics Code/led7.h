f9de669a49323e652cc8e4984c1af88ac41c3922
Collin Heist <collinheist@gmail.com>
2018-11-01 12:17:54 -0700

    Update led7.h

--- Robotics Code/led7.h
+++ Robotics Code/led7.h
@@ -3,11 +3,11 @@
 	#define __LED7_H__
 
 	/* ----------------- Necessary system include statements ----------------- */
+	#include "hardware.h"
+	#include "swDelay.h"
+
 	#include <plib.h>
 	#include <stdint.h>
-
-	#include "swDelay.h"
-	#include "hardware.h"
 
 	/* -----------------------------------------------------------------------
 	   This file defines the port and pin assignments for the 7 segment LED 
@@ -39,19 +39,19 @@
 						 AN3cfg())
 
 	/* -- Macro controls for the ON/OFF state of the LEDs. Active low LEDs --- */
-	#define SEG_CA(b);  {if(b) LATGCLR = BIT_12; else LATGSET = BIT_12;}  //RG12
-	#define SEG_CB(b);  {if(b) LATACLR = BIT_14; else LATASET = BIT_14;}  //RA14
-	#define SEG_CC(b);  {if(b) LATDCLR = BIT_6;  else LATDSET = BIT_6; }  //RD5
-	#define SEG_CD(b);  {if(b) LATGCLR = BIT_13; else LATGSET = BIT_13;}  //RG13
-	#define SEG_CE(b);  {if(b) LATGCLR = BIT_15; else LATGSET = BIT_15;}  //RG15
-	#define SEG_CF(b);  {if(b) LATDCLR = BIT_7 ; else LATDSET = BIT_7; }  //RD6
-	#define SEG_CG(b);  {if(b) LATDCLR = BIT_13; else LATDSET = BIT_13;}  //RD13
-	#define SEG_DP(b);  {if(b) LATGCLR = BIT_14; else LATGSET = BIT_14;}  //RG14
+	#define SEG_CA(b);  {if(b) LATGCLR = BIT_12; else LATGSET = BIT_12;}	//RG12
+	#define SEG_CB(b);  {if(b) LATACLR = BIT_14; else LATASET = BIT_14;}	//RA14
+	#define SEG_CC(b);  {if(b) LATDCLR = BIT_6;  else LATDSET = BIT_6; }	//RD5
+	#define SEG_CD(b);  {if(b) LATGCLR = BIT_13; else LATGSET = BIT_13;}	//RG13
+	#define SEG_CE(b);  {if(b) LATGCLR = BIT_15; else LATGSET = BIT_15;}	//RG15
+	#define SEG_CF(b);  {if(b) LATDCLR = BIT_7 ; else LATDSET = BIT_7; }	//RD6
+	#define SEG_CG(b);  {if(b) LATDCLR = BIT_13; else LATDSET = BIT_13;}	//RD13
+	#define SEG_DP(b);  {if(b) LATGCLR = BIT_14; else LATGSET = BIT_14;}	//RG14
 
-	#define DIG_AN0(b); {if(b) LATBbits.LATB12 = 1; else LATBbits.LATB12 = 0;}  //RB12
-	#define DIG_AN1(b); {if(b) LATBbits.LATB13 = 1; else LATBbits.LATB13 = 0;}  //RB13
-	#define DIG_AN2(b); {if(b) LATAbits.LATA9 = 1;  else LATAbits.LATA9 = 0; }  //RA9
-	#define DIG_AN3(b); {if(b) LATAbits.LATA10 = 1; else LATAbits.LATA10 = 0;}  //RA10
+	#define DIG_AN0(b); {if(b) LATBbits.LATB12 = 1; else LATBbits.LATB12 = 0;}	//RB12
+	#define DIG_AN1(b); {if(b) LATBbits.LATB13 = 1; else LATBbits.LATB13 = 0;}	//RB13
+	#define DIG_AN2(b); {if(b) LATAbits.LATA9 = 1;  else LATAbits.LATA9 = 0; }	//RA9
+	#define DIG_AN3(b); {if(b) LATAbits.LATA10 = 1; else LATAbits.LATA10 = 0;}	//RA10
 
 	#define SEG_LED0 {SEG_CA(1); SEG_CB(1);SEG_CC(1);SEG_CD(1);SEG_CE(1);SEG_CF(1);SEG_CG(0);SEG_DP(0);}
 	#define SEG_LED1 {SEG_CA(0); SEG_CB(1);SEG_CC(1);SEG_CD(0);SEG_CE(0);SEG_CF(0);SEG_CG(0);SEG_DP(0);}
