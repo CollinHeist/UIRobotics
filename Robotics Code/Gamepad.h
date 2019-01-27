4d9963a92262fb90523480f1f72b96dd8bfbf7f0
Damon Schafer <38117843+deschafer@users.noreply.github.com>
2019-01-26 20:35:03 -0800

    Create Gamepad.h
    Corresponding header file for the Gamepad.c library

---
+++ Robotics Code/Gamepad.h
@@ -0,0 +1,13 @@
+// 
+// File:   Gamepad.h
+// Author: Damon
+//
+// Created on January 26, 2019, 4:30 PM
+//
+
+#pragma once
+
+
+int HandleInput();	// Call to handle gamepad input
+int GamepadInit();  // Call to initialize this input library
+
